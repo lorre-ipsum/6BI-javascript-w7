14946fe6ab6caf5f2ceb7f61ec62b5137385da9e
Lorre Theunissen <145343631+lorre-ipsum@users.noreply.github.com>
2024-05-29 05:58:34 +0000

    oplossingen + nieuwe startbestanden 29/5

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// paragraaf
+console.log(
+  document.getElementById("para").innerText
+  );
+
+//image
+console.log(
+  document.getElementById("image").src
+  );
+
+//link
+console.log(
+  document.getElementById("hyperlink").href
+  );
